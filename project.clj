120f051fdf53f534acdaf9e8d100b883febca0cf
Suvash Thapaliya <suvash@gmail.com>
2016-07-21 23:01:10 +0200

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject one-time "0.1.0"
+  :author "Suvash Thapaliya"
+  :description "One Time Password (TOTP and HOTP) library for Clojure. TOTP/HOTP is widely used for Multi Factor Authentication."
+  :url "http://github.com/suvash/one-time"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [commons-codec "1.10"]
+                 [ring/ring-codec "1.0.1"]]
+  :profiles {:dev {:plugins      []}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+master"]})
