7feab729c398ee1aba1b131ef29211b7db185321
Suvash Thapaliya <suvash@gmail.com>
2017-08-05 20:42:56 +0200

    Update dependencies and run tests for more clojure versions

--- project.clj
+++ project.clj
@@ -8,11 +8,12 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [commons-codec "1.10"]
                  [ring/ring-codec "1.0.1"]
-                 [com.github.kenglxn.qrgen/javase "2.2.0"]]
+                 [com.github.kenglxn.qrgen/javase "2.3.0"]]
   :profiles {:dev {:plugins      [[lein-cloverage "1.0.6"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
   :repositories {"jitpack" "https://jitpack.io"
                  "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
@@ -20,5 +21,5 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+1.8:+master"]}
   :global-vars {*warn-on-reflection* true})
