223a7304005488fe3f67d632c7f04d7d8a758973
Suvash Thapaliya <suvash@gmail.com>
2017-12-09 11:26:23 +0100

    Bump to version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject one-time "0.4.0-SNAPSHOT"
+(defproject one-time "0.4.0"
   :author "Suvash Thapaliya"
   :description "One Time Password (TOTP and HOTP) library for Clojure. TOTP/HOTP is widely used for Two factor / Multi Factor Authentication."
   :url "http://github.com/suvash/one-time"
@@ -14,7 +14,8 @@
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}}
   :codox {:output-path "docs"
           :doc-files ["README.md"]
           :source-uri "https://github.com/suvash/one-time/blob/v{version}/{filepath}#L{line}"}
@@ -25,5 +26,5 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+1.8:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+1.8:+1.9:+master"]}
   :global-vars {*warn-on-reflection* true})
