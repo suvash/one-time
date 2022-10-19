333ae4d233298efa47d64bd93f35e0f900a471ff
Suvash Thapaliya <suvash@gmail.com>
2022-10-19 16:43:34 +0200

    Drop testing for 1.6,1.7, Test 1.12 alpha properly, Add cljfmt plugin

--- project.clj
+++ project.clj
@@ -17,9 +17,8 @@
   :plugins      [[lein-ancient "1.0.0-RC4-SNAPSHOT"]]
   :profiles {:dev    {:plugins      [[lein-cloverage "1.2.4"]
                                      [lein-codox "0.10.8"]
+                                     [lein-cljfmt "0.9.0"]
                                      [lein-ancient "1.0.0-RC4-SNAPSHOT"]]}
-             :1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8    {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9    {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10   {:dependencies [[org.clojure/clojure "1.10.0"]]}
@@ -28,16 +27,14 @@
              :1.10.3 {:dependencies [[org.clojure/clojure "1.10.3"]]}
              :1.11   {:dependencies [[org.clojure/clojure "1.11.0"]]}
              :1.11.1 {:dependencies [[org.clojure/clojure "1.11.1"]]}
-             :alpha  {:dependencies [[org.clojure/clojure "clojure-1.12.0-alpha1"]]}}
+             :alpha  {:dependencies [[org.clojure/clojure "1.12.0-alpha1"]]}}
   :codox {:output-path "docs"
           :doc-files   ["README.md"]
           :source-uri  "https://github.com/suvash/one-time/blob/v{version}/{filepath}#L{line}"}
-  :repositories [["jitpack"  "https://jitpack.io"]
+  :repositories [["jitpack" "https://jitpack.io"]
                  ["sonatype" {:url "https://oss.sonatype.org/content/repositories/releases"
                               :snapshots false
                               :releases {:checksum :fail :update :always}}]
-                 ["sonatype-snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"
-                                        :snapshots true
-                                        :releases {:checksum :fail :update :always}}]]
-  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+1.8:+1.9:+1.10:+1.10.1:+1.10.2:+1.10.3+:1.11.0:+1.11.1:+alpha"]}
+                 ["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  :aliases  {"all" ["with-profile" "+dev:+1.8:+1.9:+1.10:+1.10.1:+1.10.2:+1.10.3:+1.11:+1.11.1:+alpha"]}
   :global-vars {*warn-on-reflection* true})
