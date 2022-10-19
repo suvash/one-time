a3df586aaedd1fefb9a9fe40bdfa37adca2ac23c
Suvash Thapaliya <suvash@gmail.com>
2022-10-19 16:46:54 +0200

    Start 0.8.0-SNAPSHOT
    - major dependency updates
    - drops support for old clojure version (1.6, 1.7)
    - tests from 1.8 and upwards
    - bumps minimum clojure version to 1.10.0
    - 1.10.0 because that's the minimum on Java 8
    - Java 8 because that's the minimum LTS release supported

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject one-time "0.7.0"
+(defproject one-time "0.8.0-SNAPSHOT"
   :author "Suvash Thapaliya"
   :description "One Time Password (TOTP and HOTP) library for Clojure. TOTP/HOTP is widely used for Two factor / Multi Factor Authentication."
   :url "http://github.com/suvash/one-time"
